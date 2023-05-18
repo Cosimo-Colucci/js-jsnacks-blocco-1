e6700548468d7ff2e2611b364feed5b088d99eca
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-05-18 16:35:48 +0200

    snack2

---
+++ jsnack2/script.js
@@ -0,0 +1,19 @@
+numberList = ["Giuseppe", "Antonio", "Francesca", "Fabiola", "Colosso"]
+
+const userName = prompt("Insert your name! (the firt letter must be uppercase) ");
+let isNameFound = false;
+
+for (let i = 0; i < numberList.length ; i++) {
+    if  ( numberList [i] === userName ){
+        isNameFound = true;
+    }
+}
+
+if ( isNameFound === true){
+    console.log ("Enjoi the party");
+} else {
+    console.log ("Get away from here");
+}
+
+
+//console.log (userName);
