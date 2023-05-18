838ad2c97fb71b65c6b9af9eb9b43fa3d2e5ce3a
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-05-18 16:08:52 +0200

    snack1 no template literal

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,4 +1,9 @@
+NumberList = []
 
 for (let i = 0 ; i < 10 ; i++){
-    const userNumbers = prompt("insert a numrer");
+    const userNumbers = parseInt  (prompt ("insert a numrer"));
+    NumberList.push (userNumbers);
+    //console.log(NumberList);
 } 
+
+console.log (NumberList[0] + NumberList[1] + NumberList[2] + NumberList[3] + NumberList[4] + NumberList[5] + NumberList[6] + NumberList[7] + NumberList[8] + NumberList[9]);
