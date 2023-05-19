450f49a3cb9736f274aa4b5628af2c7758e57a6f
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-05-19 10:15:22 +0200

    second metod more generic

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -7,3 +7,17 @@ for (let i = 0 ; i < 10 ; i++){
 } 
 
 console.log (NumberList[0] + NumberList[1] + NumberList[2] + NumberList[3] + NumberList[4] + NumberList[5] + NumberList[6] + NumberList[7] + NumberList[8] + NumberList[9]);
+//problema con la mia soluzione è che se devo aumentare le volte che domando di inserire un numero devo aggiungere anche esso a somma della lista!
+
+//metodo secondo riccardo
+
+let sum = 0;
+
+for (let i = 0; i < 10; i++) {
+
+    sum += parseInt (prompt ("insert a number"));
+}
+
+console.log(sum);
+
+//metodo più generico che mi permette di modificare il ciclo senza dover modificare altre righe di codice
